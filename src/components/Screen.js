059b6fbfd8df537d408e65d6555b7a86a65641cf
Nikita <malygin.prog@yandex.ru>
2022-10-27 12:32:38 +0500

    Change Screen.js

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,7 @@
-import { Textfit } from 'react-textfit'
 import './Screen.css'
 
 const Screen = ({ value }) => {
-  return (
-    <Textfit className="screen" mode="single" max={70}>
-      {value}
-    </Textfit>
-  )
+  return <div className="screen">{value}</div>
 }
 
 export default Screen
